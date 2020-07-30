11cc75ec215730a281c971ece65de48a9a55e2a6
Mikhail2048 <68962645+Mikhail2048@users.noreply.github.com>
2020-07-30 08:59:39 +0400

    Create UserDetailsService Impl

--- src/main/java/com/example/JWTAuthenticationService/services/UserDetailsServiceImplemenation.java
+++ src/main/java/com/example/JWTAuthenticationService/services/UserDetailsServiceImplemenation.java
@@ -1,12 +1,27 @@
 package com.example.JWTAuthenticationService.services;
 
+import com.example.JWTAuthenticationService.models.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class UserDetailsServiceImplemenation implements UserDetailsService {
+
+    private UserServiceImplementation userServiceImplementation;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User fetchedUser = userServiceImplementation.findUserByUsername(username);
+        if (fetchedUser == null) {
+            log.warn("user with username : {} was not found", username);
+            throw new UsernameNotFoundException("user with username " + username + "was not found");
+        } else {
+            log.info("user with username : {} was successfully fetched from database", username);
+            return ConversionUserIntoUserDetailsUtil.convertPassedUserIntoCustomUserDetails(fetchedUser);
+        }
     }
 }
